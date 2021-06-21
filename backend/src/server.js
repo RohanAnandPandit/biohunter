18428646521b206a4f7c765f54363a05917f1517
Pandit, Rohan <rohan.pandit19@imperial.ac.uk>
2021-06-21 20:35:35 +0100

    Use correct port in server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,9 +13,7 @@ const app = express();
 
 const publicPath = path.join(__dirname, '..', 'public');
 
-// const port = process.env.PORT || 5000;
-
-const port = 5000;
+const port = process.env.PORT || 3000;
 
 app.use(cors()); // ADDED
 
